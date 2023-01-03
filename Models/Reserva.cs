5e5400db1735eaeaff42fe1a2273431e39321ad7
Antonio Melao <80098757+antoniomelao1971@users.noreply.github.com>
2023-01-02 23:15:50 -0300

    Versao final
    Versao final

---
+++ Models/Reserva.cs
@@ -0,0 +1,80 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            // *IMPLEMENTE AQUI*
+            if (Suite.Capacidade >= hospedes.Count)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                // *IMPLEMENTE AQUI*
+                Console.WriteLine("Capaciade máxima de hóspedes excedida para esta suite.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
+            // *IMPLEMENTE AQUI*
+            int qtde = 0;
+            try
+            {
+                qtde = Hospedes.Count();
+            }
+            catch (Exception e)
+            {
+               qtde = 0;
+//                throw;
+            }
+            return qtde; //Hospedes;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // TODO: Retorna o valor da diária
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            // *IMPLEMENTE AQUI*
+            decimal valor = 0.00m;
+            
+            try
+            {
+                valor = Suite.ValorDiaria  * Hospedes.Count * DiasReservados;
+            }
+            catch (Exception e)
+            {
+                valor = 0.0m;
+            }
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            // *IMPLEMENTE AQUI*
+            if (DiasReservados>=10)
+            {
+                valor = valor * 0.90m;
+            }
+
+            return valor;
+        }
+    }
+}
